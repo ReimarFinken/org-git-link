fe39ea1feb79327898ea5669e766dfe001d456cc
Reimar Finken <Reimar.Finken@gmx.de>
2009-10-05 18:06:13 +0200

    Reverting "Renamed testgitrepos to testrepository" Added source file

---
+++ org-git-link.el
@@ -0,0 +1,31 @@
+;;; org-git-link.el --- Provide org links to specific file version
+
+;; Copyright (C) 2009  Reimar Finken
+
+;; Author: Reimar Finken <reimar.finken@gmx.de>
+;; Keywords: files, calendar, hypermedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+;; Utility functions (file names etc)
+
+;; Calling git
+(provide 'org-git-link)
+;;; org-git-link.el ends here
+
